4a185ddda46b099e1b5bca607d5954c10c05fa3b
masiyematome <matomemasiye7@gmail.com>
2021-01-31 18:09:38 +0200

    Added a javascript function that moves the slides automatically and another one that moves the slides forward and backward using controls

--- main_app.js
+++ main_app.js
@@ -1,2 +1,53 @@
 //Selectors
 
+const mainContainer = document.querySelector(".main-container");
+const slides = document.querySelectorAll(".slide");
+let currentSlideIndex = 0;
+const slideInterval = setInterval(moveSlideToNext,3000);
+
+//Event Listeners
+
+mainContainer.addEventListener("click",previousToNext);
+
+//Functions
+
+function moveSlideToNext(){
+    slides[currentSlideIndex].className = "slide";
+
+    if(currentSlideIndex == slides.length-1){
+        currentSlideIndex = 0;
+    }
+
+    else{
+        currentSlideIndex = currentSlideIndex +1;
+    }
+
+    slides[currentSlideIndex].className = "slide showing";
+
+}
+
+function previousToNext(event){
+    const clickedButton = event.target;
+
+    switch(clickedButton.className){
+        case "previous-button":
+
+        slides[currentSlideIndex].className = "slide";
+
+            if(currentSlideIndex == 0){
+                currentSlideIndex = slides.length-1;
+            }
+
+            else{
+                currentSlideIndex = currentSlideIndex - 1;
+            }
+
+        slides[currentSlideIndex].className = "slide showing";
+
+            break;
+
+            case "next-button":
+                moveSlideToNext();
+                break;
+    }
+}
